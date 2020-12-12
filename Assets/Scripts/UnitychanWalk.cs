cdaab883c02c81166a3f9d20230712e03c159a49
fukuchang <aki-basketplayer21.eye@i.softbank.jp>
2020-12-12 20:59:17 +0900

    comp

---
+++ Assets/Scripts/UnitychanWalk.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public class UnitychanWalk : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private Rig _spineRig;
+    private Animator _animator;
+    private bool _isFind;
+    private bool _isArrive;
+    private float _moveSpeed = 1f;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        //Invoke("TargetFind", 7f);   
+    }
+
+    private void OnAnimatorMove()
+    {
+        if (_isFind)
+        {
+            transform.LookAt(_target);
+        }
+        if (!_isArrive)
+        {
+            transform.position += transform.forward * _moveSpeed * Time.deltaTime;
+        }
+    }
+
+    private void TargetFind()
+    {
+        _isFind = true;
+        _spineRig.weight = 0;
+        _moveSpeed = 3f;
+        _animator.SetBool("isFind", true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Finish"))
+        {
+            _isArrive = true;
+            _animator.SetBool("isArrive", true);
+        }
+    }
+}
